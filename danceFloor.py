d9f7a0134bacc759ef59ba32e5091884d736f5c1
Dylan <dwilliams349@lsr7.net>
2021-12-07 14:08:29 -0600

    ff

--- danceFloor.py
+++ danceFloor.py
@@ -11,9 +11,15 @@ length = 5
 block = 41
 mc.setBlocks(floorX, floorY, floorZ,
              floorX + width, floorY, floorZ + length, block)
-
-while floorX <= pos.x <= floorX +width and floorZ <= pos.z <= floorZ + length:
-    if block == 41:
-        block = 57
-    else:
-        block = 41
+while True:
+    pos = mc.player.getTilePos()
+    while floorX <= pos.x <= floorX +width and floorZ <= pos.z <= floorZ + length:
+        if block == 41:
+            block = 57
+        else:
+            block = 41
+    
+        mc.setBlocks(floorX, floorY, floorZ,
+                     floorX + width, floorY, floorZ + length, block)
+        pos = mc.player.getTilePos()
+        time.sleep(0.5)
---
+++ midas.py
@@ -0,0 +1,8 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+air = 0
+water = 9
+
+a = 1 While a ==1:
+    
